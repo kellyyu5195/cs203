1fdc4071ea0bbac67b7b8e063da2bfe55e224c09
kellyyu5195 <keyu@vassar.edu>
2015-12-14 21:03:07 -0500

    Finishing Hourly

--- FinalProject_Employee/EmployeeApp/src/employeeapp/Hourly.java
+++ FinalProject_Employee/EmployeeApp/src/employeeapp/Hourly.java
@@ -10,6 +10,32 @@ package employeeapp;
  *
  * @author Kelly Yu Two
  */
-public class Hourly {
+public class Hourly extends Employee{
+    
+    public Hourly (String aName, double aRate, double hrs) {
+        super(aName, 0);
+        HourlyRate = aRate;
+        Hours = hrs;
+    }
+    
+    public void setHours(double hrs) {
+        Hours = hrs;
+    }
+    
+    public void setHourlyRate(double aRate){
+        HourlyRate = aRate;
+    }
+    
+    public double getSalary() {
+        return Hours * HourlyRate;
+    }
+    
+    public String toString() {
+        return super.getName() + ": " + this.getSalary();
+    }
+    
+    private double HourlyRate;
+    private double Hours;
+    
     
 }
